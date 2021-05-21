7352896ec94a6bb40cbf118dd0cb0e528c904728
Travis Ralston <travpc@gmail.com>
2021-05-21 15:52:11 -0600

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="synapse-simple-antispam",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     description="Simple antispam for Synapse",
     include_package_data=True,
